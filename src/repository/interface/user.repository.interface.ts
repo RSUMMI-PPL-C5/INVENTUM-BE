34b03763318be2ee763ee16fbea7966a589aa724
fikririsyad <fikri.risyad10@gmail.com>
2025-03-06 17:47:55 +0700

    [RED] Add tests for getting filtered users in userRepository

--- src/repository/interface/user.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { UserDTO } from "../../dto/user.dto";
-import { User } from "@prisma/client";
-export interface IUserRepository {
-    getUsers(): Promise<UserDTO[]>;
-    findByUsername(username: string): Promise<User | null>;
-}
