b914fa609253eee2ba9611c03b4eaa76baff5ae1
Fec16 <jasonkentwinata@gmail.com>
2025-03-11 15:02:33 +0700

    Remove redundant IUser

--- src/services/interface/user.service.interface.ts
+++ src/services/interface/user.service.interface.ts
@@ -3,21 +3,3 @@ import { User } from "@prisma/client";
 export interface IUserService {
   searchUser(name: string): Promise<User[]>;
 }
-
-export interface IUser {
-  id: string;
-  email: string;
-  username: string;
-  password: string;
-  role?: string;
-  fullname?: string;
-  nokar: string;
-  divisiId?: number;
-  waNumber?: string;
-  createdBy: number;
-  createdOn: Date;
-  updatedBy?: number;
-  updatedOn?: Date;
-  deletedBy?: number;
-  deletedOn?: Date;
-}
