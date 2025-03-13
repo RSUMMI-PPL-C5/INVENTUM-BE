4fbf7d1300ce266b40bf3e1d25cc90be4710f454
azmyar <azmy.arya.rizaldi@gmail.com>
2025-03-13 13:47:49 +0700

    fix: filter validation

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
 import { addUserValidation } from '../validations/adduser.validation';
+import { userFilterQueryValidation } from '../validations/userfilterquery.validation';
 
 const router = Router();
 const userController = new UserController();
 
-router.get('/', userController.getUsers);
+router.get('/', userFilterQueryValidation, userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', addUserValidation, userController.addUser);
 router.put('/:id', userController.updateUser);
