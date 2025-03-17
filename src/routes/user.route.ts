c07f4add67f6da0708fc66bca1ecc398dd8fdd30
azmyar <azmy.arya.rizaldi@gmail.com>
2025-03-17 22:24:09 +0700

    feat: verify token on each API hit

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import verifyToken from '../middleware/verifyToken';
 import { addUserValidation } from '../validations/adduser.validation';
 import { userFilterQueryValidation } from '../validations/userfilterquery.validation';
 
 const router = Router();
 const userController = new UserController();
 
-router.get('/', userFilterQueryValidation, userController.getUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', addUserValidation, userController.addUser);
-router.put('/:id', userController.updateUser);
-router.delete('/:id', userController.deleteUser);
+router.get('/', verifyToken, userFilterQueryValidation, userController.getUsers);
+router.get('/:id', verifyToken, userController.getUserById);
+router.post('/', verifyToken, addUserValidation, userController.addUser);
+router.put('/:id', verifyToken, userController.updateUser);
+router.delete('/:id', verifyToken, userController.deleteUser);
 
 export default router;
