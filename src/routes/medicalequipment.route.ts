20870bdd29de099b5f4dd7f68af87aa83aee83ec
Fec16 <jasonkentwinata@gmail.com>
2025-03-24 22:10:21 +0700

    [REFACTOR] Remove unused import

--- src/routes/medicalequipment.route.ts
+++ src/routes/medicalequipment.route.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import MedicalEquipmentController from "../controllers/medicalequipment.controller";
-import verifyToken from "../middleware/verifyToken";
 
 const router = Router();
 const medicalEquipmentController = new MedicalEquipmentController();
