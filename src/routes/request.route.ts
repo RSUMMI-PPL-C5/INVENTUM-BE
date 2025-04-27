55afcb92c228cb4581b97e1e96c7e45608822f1a
fauzanjaisyu <fauzanjaisyu@gmail.com>
2025-04-28 01:47:44 +0700

    [GREEN] Implement request maintenance route

--- src/routes/request.route.ts
+++ src/routes/request.route.ts
@@ -8,6 +8,13 @@ const requestController = new RequestController();
 // Get all requests
 router.get("/all", verifyToken, requestController.getAllRequests);
 
+// Get all maintenance requests
+router.get(
+  "/maintenance",
+  verifyToken,
+  requestController.getAllRequestMaintenance,
+);
+
 // Get request by ID
 router.get("/:id", verifyToken, requestController.getRequestById);
 
